374eec3dead4db25577718b8b4b2bfea3d83297e
aaasjjwieiie-art <aaasjjwieiie@gmail.com>
2026-04-27 20:01:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -166,4 +166,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_data()
-    app.run(debug=True, port=5000)
+    # Изменяем host на '0.0.0.0', чтобы принимать внешние запросы
+    app.run(host='0.0.0.0', port=5000)
